dbf3e7d10c44e9b4a319d003d703acbceed6d314
yzr95924 <zryang@cse.cuhk.edu.hk>
2020-10-14 03:09:44 +0800

    update

---
+++ include/SSLConnect.h
@@ -0,0 +1,25 @@
+/**
+ * @file SSLConnect.h
+ * @author Zuoru YANG (zryang@cse.cuhk.edu.hk)
+ * @brief define the SSL connection interface 
+ * @version 0.1
+ * @date 2020-10-11
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#ifndef MY_CODEBASE_SSL_CONNECT_H
+#define MY_CODEBASE_SSL_CONNECT_H 
+
+#include "define.h"
+
+
+enum ConnectSide_t {ClientSide, ServerSide};
+
+
+class SSLConnect {
+    
+
+};
+#endif // !1
