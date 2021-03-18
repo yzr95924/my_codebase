dcee5f47513b47766e13f8ace893e98f542fb250
yzr95924 <zryang@cse.cuhk.edu.hk>
2021-03-18 21:22:05 +0800

    add the server/client example

---
+++ src/app/client.cc
@@ -0,0 +1,49 @@
+/**
+ * @file client.cc
+ * @author Zuoru YANG (zryang@cse.cuhk.edu.hk)
+ * @brief implement a simple client program
+ * @version 0.1
+ * @date 2021-01-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "../../include/sslConnection.h"
+#include "../../include/configure.h"
+
+
+int main(int argc, char* argv[])
+{
+    pair<int, SSL*> newSSLConnectionRecord;
+    // config the parameter
+    int port = 16666;
+    string serverIp = "127.0.0.1";
+    SSLConnection* sslConnectionHandler = new SSLConnection(serverIp, port, IN_CLIENTSIDE);
+    newSSLConnectionRecord = sslConnectionHandler->ConnectSSL();
+
+    int sendData = 95924;
+    fprintf(stderr, "Send data is %d\n", sendData);
+    if (!sslConnectionHandler->SendData(newSSLConnectionRecord.second, (uint8_t*)&sendData, sizeof(int))) {
+        fprintf(stderr, "Send data error.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    sendData = 10000;    
+    fprintf(stderr, "Send data is %d\n", sendData);
+    if (!sslConnectionHandler->SendData(newSSLConnectionRecord.second, (uint8_t*)&sendData, sizeof(int))) {
+        fprintf(stderr, "Send data error.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // if (!sslConnectionHandler->ReceiveData(newSSLConnectionRecord.second, (uint8_t*)&recvData, recvDataSize)) {
+    //     fprintf(stderr, "The connection is closed.\n");
+    // }
+
+    // fprintf(stderr, "Receive data is %d\n", recvData);
+    
+    sslConnectionHandler->Finish(newSSLConnectionRecord);
+    delete sslConnectionHandler;
+    fprintf(stderr, "Finish the test.\n");
+    return 0;
+}
---
+++ src/app/server.cc
@@ -0,0 +1,63 @@
+/**
+ * @file server.cc
+ * @author Zuoru YANG (zryang@cse.cuhk.edu.hk)
+ * @brief a simple server program to test ssl connection
+ * @version 0.1
+ * @date 2021-01-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <signal.h> // to catch the signal
+#include "../../include/sslConnection.h"
+
+SSLConnection* sslHandler;
+
+void CTRLC(int s) {
+    fprintf(stderr, "Terminate the server with keyboard interrupt, caught signal %d\n", s);
+    delete sslHandler;
+    fprintf(stderr, "Finish the test.\n");
+    exit(EXIT_SUCCESS);
+    return ;
+}
+
+
+int main(int argc, char* argv[])
+{
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = SIG_IGN;
+    sigaction(SIGPIPE, &sigIntHandler, 0);
+
+    sigIntHandler.sa_handler = CTRLC;
+    sigaction(SIGKILL, &sigIntHandler, 0);
+    sigaction(SIGINT, &sigIntHandler, 0);
+    
+    string serverIp = "127.0.0.1";
+    int port = 16666;
+    sslHandler = new SSLConnection(serverIp, port, IN_SERVERSIDE);
+    pair<int, SSL*> connectionRecord;
+
+    int recvData;
+    uint32_t recvDataSize;
+
+    while (true) {
+        connectionRecord = sslHandler->ListenSSL();
+        while (true) {
+            if (!sslHandler->ReceiveData(connectionRecord.second, (uint8_t*)&recvData, recvDataSize)) {
+                fprintf(stderr, "The connection is closed.\n");
+                sslHandler->ClearAcceptedClientSd(connectionRecord.second);
+                break;
+            }
+            fprintf(stderr, "Receive data is %d\n", recvData);
+            // recvData += 100;
+            // if (!sslHandler->SendData(connectionRecord.second, (uint8_t*)&recvData, sizeof(int))) {
+            //     fprintf(stderr, "Send data error.\n");
+            //     exit(EXIT_FAILURE);
+            // }
+        }
+        fprintf(stderr, "Finish the current connection, wait for other connections.\n");
+    }
+    
+    return 0;
+}
