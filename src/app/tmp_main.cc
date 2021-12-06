3d46944d8af22483eb5206deee705ebf2eb95c6e
yzr95924 <zryang@cse.cuhk.edu.hk>
2021-12-07 02:11:09 +0800

    update

--- src/app/tmp_main.cc
+++ src/app/tmp_main.cc
@@ -12,8 +12,30 @@
 #include "../../include/std_learn/lambda_learn.h"
 #include "../../include/std_learn/multi_thread_learn.h"
 
+static inline uint64_t rotl(const uint64_t x, int k) {
+	return (x << k) | (x >> (64 - k));
+}
+
+static uint64_t s[2] = {1};
+
+uint64_t next(void) {
+	const uint64_t s0 = s[0];
+	uint64_t s1 = s[1];
+	const uint64_t result = s0 + s1;
+
+	s1 ^= s0;
+	s[0] = rotl(s0, 24) ^ s1 ^ (s1 << 16); // a, b
+	s[1] = rotl(s1, 37); // c
+
+	return result;
+}
+
 int main(int argc, char* argv[]) {
-    MultiThreadLearning test;
-    test.FutureTest();
+    next();
+    tool::PrintBinaryArray((uint8_t*)s, sizeof(uint64_t) * 2);
+    next();
+    tool::PrintBinaryArray((uint8_t*)s, sizeof(uint64_t) * 2);
+    next();
+    tool::PrintBinaryArray((uint8_t*)s, sizeof(uint64_t) * 2);
     return 0;
 }
